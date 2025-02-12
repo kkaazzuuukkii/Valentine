31dc36062b9f94964838d52d70593a5c8b6bb847
Kazuki <bogdankonoshko@gmail.com>
2025-02-12 22:33:27 +0200

    ???

--- main.js
+++ main.js
@@ -3,13 +3,37 @@ const no = document.getElementById('no');
 const main = document.getElementsByClassName('container')[0];
 const yes1 = document.getElementsByClassName('yes')[0];
 const no1 = document.getElementsByClassName('no')[0];
+let count1 = 0;
+let count2 = 0;
 
-addEventListener('click', (e) => {
+yes.addEventListener('click', (e) => {
   if (e.target === yes) {
+    count1++;
+    yes.innerHTML = "Are you sure?"
+  }
+  if (count1 === 2) {
     yes1.classList.remove('inactive');
     main.classList.add('inactive');
-  } else if (e.target === no) {
+  }
+})
+
+no.addEventListener('click', (e) => {
+  if (e.target === no) {
+    count2++;
+    no.innerHTML = "Are you sure?"
+  }
+  if (count2 === 2) {
     no1.classList.remove('inactive');
     main.classList.add('inactive');
   }
 })
+
+// addEventListener('click', (e) => {
+//   if (e.target === yes) {
+//     yes1.classList.remove('inactive');
+//     main.classList.add('inactive');
+//   } else if (e.target === no) {
+//     no1.classList.remove('inactive');
+//     main.classList.add('inactive');
+//   }
+// })
